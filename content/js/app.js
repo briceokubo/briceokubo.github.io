45b419e114c4f9dd90f875248065b2e0fdf0afcf
briceokubo <bricet@hawaii.edu>
2017-02-06 01:27:45 -1000

    Added images to work in app.js

--- content/js/app.js
+++ content/js/app.js
@@ -2,10 +2,10 @@ var app = angular.module("myApp", []);
 
 app.controller("workCtrl", function ($scope) {
    $scope.works = [
-      { image: "http://placehold.it/300x300", title: "TecHub website", description: "New web section for UH Manoa Bookstore", tag: "Web Design"},
-      { image: "http://placehold.it/300x300", title: "TecHub Logo", description: "Creating a new brand for Computer department", tag: "Branding"},
-      { image: "http://placehold.it/300x300", title: "TheDeepEnd Logo", description: "Branding TheDeepEnd website logo", tag: "Branding"},
-      { image: "http://placehold.it/300x300", title: "Carbon Neutral", description: "Case study of UI/UX web app", tag: "Front-End"},
-      { image: "http://placehold.it/300x300", title: "Hike Aloha", description: "UI/UX and Branding for 24-hour hackathon winning ", tag: "Front End and Branding"},
+      { image: "content/images/techub.jpg", title: "TecHub website", description: "New web page for UH Manoa Bookstore", tag: "Web Design"},
+      { image: "content/images/techublogo.svg", title: "TecHub Logo", description: "Creating a new brand for Computer department", tag: "Branding"},
+      { image: "content/images/deependHero.svg", title: "TheDeepEnd Logo", description: "Branding TheDeepEnd website logo", tag: "Branding"},
+      { image: "content/images/cn.svg", title: "Carbon Neutral", description: "Case study of UI/UX web app", tag: "Front-End"},
+      { image: "content/images/hikealoha.svg", title: "Hike Aloha", description: "UI/UX and Branding for 24-hour hackathon winning ", tag: "Front End and Branding"},
    ];
 })
