db5a986557228d04e57a5c2ebd8ddc5764249d9f
briceokubo <bricet@hawaii.edu>
2017-07-14 15:21:56 -1000

    Added Modern Compass to workCtrl

--- content/js/app.js
+++ content/js/app.js
@@ -2,6 +2,7 @@ var app = angular.module("myApp", []);
 
 app.controller("workCtrl", function ($scope) {
    $scope.works = [
+      { image: "content/images/MCicon.svg", title: "Modern Compass", description: "Exploring outdoors the safe way", a: "modernCompass.html", tag: "Web Design"},
       { image: "content/images/techub.jpg", title: "TecHub website", description: "New web page for UH Manoa Bookstore", a: "techub.html", tag: "Web Design"},
       { image: "content/images/cn.svg", title: "Carbon Neutral", description: "Case study of UI/UX web app", a: "CNchallenge.html", tag: "Front-End"},
       { image: "content/images/hikealoha.svg", title: "Hike Aloha", description: "UI/UX and Branding for 24-hour hackathon winning ", a: "hikealoha.html", tag: "Front End and Branding"},
