88df5d430a6e00ab396e2bf328f0d91530e8a0ea
briceokubo <bricet@hawaii.edu>
2017-08-21 14:21:36 -1000

    2.3.0
    -Edited images
    -Added Resume
    -update mobile friendly
    -text edited

--- content/js/app.js
+++ content/js/app.js
@@ -5,7 +5,7 @@ app.controller("workCtrl", function ($scope) {
       { image: "content/images/homepage/MCicon.svg", title: "Modern Compass", description: "Exploring outdoors the safe way", a: "modernCompass.html", tag: "Web Design"},
       { image: "content/images/homepage/techub.jpg", title: "TecHub website", description: "New web page for UH Manoa Bookstore", a: "techub.html", tag: "Web Design"},
       { image: "content/images/homepage/CNhero.png", title: "Carbon Neutral", description: "Case study of UI/UX web app", a: "CNchallenge.html", tag: "Front-End"},
-      { image: "content/images/homepage/hikealoha.svg", title: "Hike Aloha", description: "UI/UX and Branding for 24-hour hackathon winning ", a: "hikealoha.html", tag: "Front End and Branding"},
+      { image: "content/images/homepage/hikealoha.svg", title: "Hike Aloha", description: "UI/UX and Branding for 24-hour hackathon", a: "hikealoha.html", tag: "Front End and Branding"},
       { image: "content/images/homepage/techublogo.svg", title: "TecHub Logo", description: "Creating a new brand for Computer department", a: "techublogo.html", tag: "Branding"},
       { image: "content/images/homepage/deependHero.svg", title: "TheDeepEnd Logo", description: "Branding TheDeepEnd website logo", a: "deepend.html", tag: "Branding"},
    ];
