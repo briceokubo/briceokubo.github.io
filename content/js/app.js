192c335cbbf1b628c1f8f781f793c7c227083531
briceokubo <bricet@hawaii.edu>
2017-09-03 17:39:06 -1000

    Added parts to main angular json table

--- content/js/app.js
+++ content/js/app.js
@@ -2,11 +2,11 @@ var app = angular.module("myApp", []);
 
 app.controller("workCtrl", function ($scope) {
    $scope.works = [
-      { image: "content/images/homepage/MCicon.svg", title: "Modern Compass", description: "Exploring outdoors the safe way", a: "modernCompass.html", tag: "Web Design"},
-      { image: "content/images/homepage/techub.jpg", title: "TecHub website", description: "New web page for UH Manoa Bookstore", a: "techub.html", tag: "Web Design"},
-      { image: "content/images/homepage/CNhero.png", title: "Carbon Neutral", description: "Case study of UI/UX web app", a: "CNchallenge.html", tag: "Front-End"},
-      { image: "content/images/homepage/hikealoha.svg", title: "Hike Aloha", description: "UI/UX and Branding for 24-hour hackathon", a: "hikealoha.html", tag: "Front End and Branding"},
-      { image: "content/images/homepage/techublogo.svg", title: "TecHub Logo", description: "Creating a new brand for Computer department", a: "techublogo.html", tag: "Branding"},
-      { image: "content/images/homepage/deependHero.svg", title: "TheDeepEnd Logo", description: "Branding TheDeepEnd website logo", a: "deepend.html", tag: "Branding"},
+      { image: "content/images/homepage/MCicon2.svg", title: "Modern Compass", description: "Exploring outdoors the safe way", a: "modernCompass.html", tag: "Web Design", color: "tagWD"},
+      { image: "content/images/homepage/techub2.png", title: "TecHub website", description: "New web page for UH Manoa Bookstore", a: "techub.html", tag: "Web Design", color: "tagWD"},
+      { image: "content/images/homepage/CN2.png", title: "Carbon Neutral", description: "Case study of UI/UX web app", a: "CNchallenge.html", tag: "Web Design", color: "tagWD"},
+      { image: "content/images/homepage/hikealoha2.svg", title: "Hike Aloha", description: "UI/UX and Branding for 24-hour hackathon", a: "hikealoha.html", tag: "Mobile Design", color: "tagMD"},
+      { image: "content/images/homepage/techublogo.png", title: "TecHub Logo", description: "Creating a new brand for Computer department", a: "techublogo.html", tag: "Branding", color: "tagB"},
+      { image: "content/images/homepage/deependHero.svg", title: "TheDeepEnd Logo", description: "Branding TheDeepEnd website logo", a: "deepend.html", tag: "Branding", color: "tagB"},
    ];
 })
