851f5844deee7494d191fef20645c767f803dbfb
briceokubo <bricet@hawaii.edu>
2017-09-18 17:08:41 -1000

    Included footer js

--- content/js/button.js
+++ content/js/button.js
@@ -1,12 +1,44 @@
+//  OLD Nav Layout
+// $(document).ready(function(){
+//    $(".contactBtn").hover(function () {
+//    $(this).toggleClass("btn_hover");
+//    });
+// });
+//
+// $(document).ready(function(){
+//    $(".logo").hover(function () {
+//    $(this).toggleClass("logo_hover");
+//    });
+// });
 
 $(document).ready(function(){
-   $(".contactBtn").hover(function () {
-   $(this).toggleClass("btn_hover");
+   $("#mail") .mouseover(function () {
+      this.src= "content/images/footer/mail_hover.svg"
+   }).mouseout(function () {
+      this.src= "content/images/footer/mail.svg"
    });
 });
 
 $(document).ready(function(){
-   $(".logo").hover(function () {
-   $(this).toggleClass("logo_hover");
+   $("#link") .mouseover(function () {
+      this.src= "content/images/footer/linkedin_hover.svg"
+   }).mouseout(function () {
+      this.src= "content/images/footer/linkedin.svg"
+   });
+});
+
+$(document).ready(function(){
+   $("#insta") .mouseover(function () {
+      this.src= "content/images/footer/instagram_hover.svg"
+   }).mouseout(function () {
+      this.src= "content/images/footer/instagram.svg"
+   });
+});
+
+$(document).ready(function(){
+   $("#git") .mouseover(function () {
+      this.src= "content/images/footer/git_hover.svg"
+   }).mouseout(function () {
+      this.src= "content/images/footer/git.svg"
    });
 });
