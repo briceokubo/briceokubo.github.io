1e2364694e8a0f12107120a661c60b27bf9ccf6b
briceokubo <bricet@hawaii.edu>
2017-09-20 17:28:07 -1000

    Add if screen size scenario

--- content/js/close.js
+++ content/js/close.js
@@ -1,15 +1,10 @@
 //Desktop view close button
  $(document).scroll(function(){
     $(".closeButton").css("top",Math.max(80,700-$(this).scrollTop()));
+    if($(window).width() <= 1024){
+      $(".closeButton").css("top",Math.max(20,450-$(this).scrollTop()));
+    }
  });
-
-// //Mobile view close button
-// $(document).scroll(function(){
-//    	if($(window).width() <= 700){
-//     $(".closeButton").css("bottom",Math.max(100,650-$(this).scrollTop()));
-//    }
-// });
-
 
 $(document).ready(function(){
    $(".xbutton").hover(function () {
