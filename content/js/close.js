6790d6046d9d6f108031f71bdbb2f139c269d464
briceokubo <bricet@hawaii.edu>
2017-09-20 14:56:44 -1000

    Added Comments

--- content/js/close.js
+++ content/js/close.js
@@ -1,6 +1,15 @@
-$(document).scroll(function(){
+//Desktop view close button
+ $(document).scroll(function(){
     $(".closeButton").css("top",Math.max(80,700-$(this).scrollTop()));
-});
+ });
+
+// //Mobile view close button
+// $(document).scroll(function(){
+//    	if($(window).width() <= 700){
+//     $(".closeButton").css("bottom",Math.max(100,650-$(this).scrollTop()));
+//    }
+// });
+
 
 $(document).ready(function(){
    $(".xbutton").hover(function () {
