32e4290b42e65b25979689d01ded8757fbf25c14
briceokubo <bricet@hawaii.edu>
2017-09-21 17:18:24 -1000

    Adjust starting point of closeButton

--- content/js/close.js
+++ content/js/close.js
@@ -1,6 +1,6 @@
 //Desktop view close button
  $(document).scroll(function(){
-    $(".closeButton").css("top",Math.max(80,700-$(this).scrollTop()));
+    $(".closeButton").css("top",Math.max(80,675-$(this).scrollTop()));
     if($(window).width() <= 1024){
       $(".closeButton").css("top",Math.max(20,450-$(this).scrollTop()));
     }
