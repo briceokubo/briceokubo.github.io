a7e8fe753cfdcb2085349113885fc7aad4141fbf
briceokubo <bricet@hawaii.edu>
2017-09-21 17:17:55 -1000

    Include scrollTo to each ng-click function

--- content/js/app.js
+++ content/js/app.js
@@ -14,47 +14,92 @@ app.controller("workCtrl", function ($scope) {
 app.controller('sectionCtrl', ['$scope', function($scope) {
     $scope.section = 'views/homepage/offer.html';
 
+
     $scope.aboutSection = function() {
         $scope.section = 'views/homepage/about.html';
+        window.scrollTo(0, 550);
+        if($(window).width() <= 1024){
+            window.scrollTo(0, 400);
+        }
     };
 
     $scope.offerSection = function() {
         $scope.section = 'views/homepage/offer.html';
+        window.scrollTo(0, 550);
+        if($(window).width() <= 1024){
+            window.scrollTo(0, 400);
+        }
     };
 
     $scope.workSection = function() {
         $scope.section = 'views/homepage/work.html';
+        window.scrollTo(0, 550);
+        if($(window).width() <= 1024){
+            window.scrollTo(0, 400);
+        }
     };
 
     $scope.contactSection = function() {
       $scope.section = 'views/homepage/contact.html';
+      window.scrollTo(0, 550);
+      if($(window).width() <= 1024){
+          window.scrollTo(0, 400);
+      }
   };
 
    $scope.MCwork = function() {
       $scope.section = 'views/workBody/MCbody.html';
+      window.scrollTo(0, 550);
+      if($(window).width() <= 1024){
+          window.scrollTo(0, 400);
+      }
 };
 
    $scope.CNwork = function() {
       $scope.section = 'views/workBody/CNbody.html';
+      window.scrollTo(0, 550);
+      if($(window).width() <= 1024){
+          window.scrollTo(0, 400);
+      }
 };
 
    $scope.THwork = function() {
       $scope.section = 'views/workBody/techubBody.html';
+      window.scrollTo(0, 550);
+      if($(window).width() <= 1024){
+          window.scrollTo(0, 400);
+      }
 };
 
    $scope.HAwork = function() {
       $scope.section = 'views/workBody/HAbody.html';
+      window.scrollTo(0, 550);
+      if($(window).width() <= 1024){
+          window.scrollTo(0, 400);
+      }
 };
 
    $scope.THLwork = function() {
       $scope.section = 'views/workBody/techubLogoBody.html';
+      window.scrollTo(0, 550);
+      if($(window).width() <= 1024){
+          window.scrollTo(0, 400);
+      }
 };
 
    $scope.ABwork = function() {
       $scope.section = 'views/workBody/ABbody.html';
+      window.scrollTo(0, 550);
+      if($(window).width() <= 1024){
+          window.scrollTo(0, 400);
+      }
 };
 
    $scope.resumeSection = function() {
       $scope.section = 'views/homepage/resume.html';
+      window.scrollTo(0, 550);
+      if($(window).width() <= 1024){
+          window.scrollTo(0, 400);
+      }
 };
 }]);
