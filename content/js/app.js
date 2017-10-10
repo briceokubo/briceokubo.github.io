83f78e098f580f2c786033e43ddeac9ac57e8890
briceokubo <bricet@hawaii.edu>
2017-10-09 16:30:07 -1000

    Edit work text content

--- content/js/app.js
+++ content/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module("myApp", []);
 
 app.controller("workCtrl", function ($scope) {
    $scope.works = [
-      { image: "content/images/homepage/MCicon2.svg", title: "Modern Compass", description: "Exploring outdoors the safe way", a: "modernCompass.html", tag: "Web Design", color: "tagWD", tag2: "Mobile Design", color2: "tagMD", workBtn: "views/workBtn/MCbutton.html"},
+      { image: "content/images/homepage/MCicon2.svg", title: "Modern Compass", description: "App for exploring outdoors the safe way", a: "modernCompass.html", tag: "Web Design", color: "tagWD", tag2: "Mobile Design", color2: "tagMD", workBtn: "views/workBtn/MCbutton.html"},
       { image: "content/images/homepage/techub2.png", title: "TecHub website", description: "New web page for UH Manoa Bookstore", a: "techub.html", tag: "Web Design", color: "tagWD", tag2: "", color2: "", workBtn: "views/workBtn/THbutton.html"},
       { image: "content/images/homepage/CN2.png", title: "Carbon Neutral", description: "Case study of UI/UX web app", a: "CNchallenge.html", tag: "Web Design", color: "tagWD", tag2: "", color2: "", workBtn: "views/workBtn/CNbutton.html"},
       { image: "content/images/homepage/hikealoha2.svg", title: "Hike Aloha", description: "UI/UX and Branding for 24-hour hackathon", a: "hikealoha.html", tag: "Mobile Design", color: "tagMD", tag2: "Branding", color2: "tagB", workBtn: "views/workBtn/HAbutton.html"},
